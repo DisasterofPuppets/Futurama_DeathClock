25265c2852bdee80120a20c4c5668f136c985511
Disaster of Puppets <62915210+DisasterofPuppets@users.noreply.github.com>
2020-08-03 14:40:25 +1000

    Add files via upload

---
+++ bot_credentialsempty.h
@@ -0,0 +1,32 @@
+#ifndef BOT_CREDENTIALS_H
+#define BOT_CREDENTIALS_H
+
+#define WIFI_SSID "wifiname"
+#define WIFI_PASS "password"
+
+/* Twitch WebSocket URL */
+#define TWITCH_WEBSOCKET_URL "wss://irc-ws.chat.twitch.tv:443"
+
+/* Account name you are using to run bot */
+#define BOT_ACCOUNT_NAME "Yourchannelorbotname"
+/* If different from your channel name you must have this as a seperate registered 
+twitch account /*
+
+/* Twitch OAUTH TOKEN (https://twitchapps.com/tmi/) If you have used an account    
+different to your channel name you will need to use that to logon and obtain the oauth*/
+#define TWITCH_OAUTH_TOKEN "oauth:"
+
+/* Channel you want to run the bot in */
+#define CONNECT_CHANNEL_NAME "Channel"
+
+/* Don't change unless you know what you want to do. */
+#define CAP_REQ_STR "twitch.tv/tags twitch.tv/commands twitch.tv/membership"
+
+#define ROCK_ID ""
+#define PAPER_ID ""
+#define SCISSORS_ID ""
+#define DC_ID ""
+
+
+
+#endif
